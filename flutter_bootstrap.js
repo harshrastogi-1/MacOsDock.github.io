08bf9a282c1e7f5154b0de671bcdbff68bbe1002
Harsh Rastogi <harsh.rastogi@kutumbapp.com>
2024-12-04 14:04:08 +0530

    index changes

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"db49896cf25ceabc44096d5f088d86414e05a7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2244784161"
+    serviceWorkerVersion: "940994185"
   }
 });
--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "a46044765a6849198538fc2e2e5f8151",
+const RESOURCES = {"flutter_bootstrap.js": "b275208bbb3603e3cc6983df58628b87",
 "version.json": "df908da6e0ffc15ade28b42d37f9fda9",
 "index.html": "e0ee5e8837cd51e59d0ecd1d1f90d4c9",
 "/": "e0ee5e8837cd51e59d0ecd1d1f90d4c9",
